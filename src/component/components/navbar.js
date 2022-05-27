d53c9105b593f682ab1d37540fe22f28e4e9d336
JaMes-pong <76243561+JaMes-pong@users.noreply.github.com>
2022-05-27 13:07:56 +0800

    navbar update

--- src/component/components/navbar.js
+++ src/component/components/navbar.js
@@ -3,13 +3,39 @@ import { Link } from 'react-router-dom';
 
 function navbar() {
     return (
-        <nav class="navbar navbar-expand-sm navbar-light" id="navbar">
-            <div class="mx-auto d-sm-flex d-block flex-sm-nowrap">
-                {/*  */}
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample11" aria-expanded="false" aria-label="Toggle navigation">
+        // <nav class="navbar navbar-expand-sm navbar-light" id="navbar">
+        //     <div class="mx-auto d-sm-flex d-block flex-sm-nowrap">
+        //         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample11" aria-expanded="false" aria-label="Toggle navigation">
+        //             <span class="navbar-toggler-icon"></span>
+        //         </button>
+        //         <div class="collapse navbar-collapse text-center" id="navbarsExample11">
+        //             <ul class="navbar-nav">
+        //                 <li class="nav-item">
+        //                     <Link to="/" class="nav-link">Home</Link>
+        //                 </li>
+        //                 <li class="nav-item">
+        //                     <Link to="/map" class="nav-link">Map</Link>
+        //                 </li>
+        //                 <li class="nav-item">
+        //                     <Link to="/gallery" class="nav-link">Gallery</Link>
+        //                 </li>
+        //                 <li class="nav-item">
+        //                     <Link to="/about" class="nav-link">About</Link>
+        //                 </li>
+        //                 <li class="nav-item">
+        //                     <Link to="/joinus" class="nav-link">Join Us</Link>
+        //                 </li>
+        //             </ul>
+        //         </div>
+        //     </div>
+        // </nav>
+        <nav class="navbar navbar-expand-lg navbar-light" id="navbar">
+            <div class="container-fluid">
+                {/* <Link to="/" class="navbar-brand" id="logo">Z</Link> */}
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse text-center" id="navbarsExample11">
+                <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
                     <ul class="navbar-nav">
                         <li class="nav-item">
                             <Link to="/" class="nav-link">Home</Link>
